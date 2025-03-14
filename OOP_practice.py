6ea4122b725aea0e29cde064aba1e6316d07f604
SachemCode <110540700+SachemCode@users.noreply.github.com>
2025-03-14 14:59:23 +0000

    Add files via upload

---
+++ OOP_practice.py
@@ -0,0 +1,15 @@
+class Product:
+    def __init__(self, name, price, cost, quantity):
+        self.name = name
+        self.price = price  # Selling price
+        self.cost = cost  # Buying cost
+        self.quantity = quantity
+
+    def update_stock(self, amount):
+        self.quantity += amount
+
+
+class Department:
+    def __init__(self, name):
+        self.name = name
+
